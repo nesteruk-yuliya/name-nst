b8589f6f88b1514e95988363af6dc750f3822ead
nesteruk-yuliya <yuliya.nesteruk@vrpconsulting.com>
2023-12-20 15:49:06 +0300

    init commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+   //Напишите в отличном от Main классе метод sayNumberMultipliedByTen,
+        // который принимает аргумент типа double
+        // и выводит на экран его значение, умноженное в десять раз.
+
+    }
+}
